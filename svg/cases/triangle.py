df242af4f53d89d19d486692374a6e8c00aaf4bd
Kirill Vinogradov <vikian050194@gmail.com>
2022-10-09 18:16:12 +0200

    add equilateral triangle type

--- svg/cases/triangle.py
+++ svg/cases/triangle.py
@@ -1,7 +1,15 @@
 from typing import List
 
+import math
+from enum import Enum
+
 from svg.shapes import Polygon, Shape
 from svg.cases.abstract import AbstractCase
+
+
+class Type(str, Enum):
+    RANDOM = "random"
+    EQUILATERAL = "equilateral"
 
 
 class TriangleCase(AbstractCase):
@@ -9,7 +17,8 @@ class TriangleCase(AbstractCase):
     def name(self) -> str:
         return "triangle"
 
-    def draw(self, count: int, edge: int) -> List[Shape]:
+    def draw(self, count: int, type: Type) -> List[Shape]:
+            edge = 3
             shapes = []
     
             sc = self.palette.get_color()
@@ -20,9 +29,37 @@ class TriangleCase(AbstractCase):
                 pg.stroke = sc
                 pg.stroke_width = 5
                 pg.fill = fc
-                for _ in range(0, edge):
-                    x, y = self.rm.get_pair()
-                    pg.append(x, y)
+                if type == Type.RANDOM:
+                    for _ in range(0, edge):
+                        x, y = self.rm.get_pair()
+                        pg.append(x, y)
+                if type == Type.EQUILATERAL:
+                    xa, ya = self.rm.get_pair()
+                    xb, yb = self.rm.get_pair()
+                    r2 = ((xa-xb)**2)+((ya-yb)**2)
+                    r = math.sqrt(r2)
+
+                    la = (xb-xa)/(ya-yb)
+                    lb = (xa**2+ya**2-xb**2-yb**2)/(2*(ya-yb))
+                    a = 1 + la**2
+                    b = 2*la*(lb-ya)-2*xa
+                    c = xa**2+(lb-ya)**2-r2
+                    d2 = b**2-4*a*c
+                    d = math.sqrt(d2) 
+
+                    xc_1 = ((-1)*b+d)/(2*a)
+                    xc_2 = ((-1)*b-d)/(2*a)
+
+                    yc_1 = la*xc_1+lb
+                    yc_2 = la*xc_2+lb
+
+                    xc = round(xc_1, 2)
+                    yc = round(yc_1, 2)
+
+                    pg.append(xa, ya)
+                    pg.append(xb, yb)
+                    pg.append(xc, yc)
+
                 shapes.append(pg)
 
             return shapes
--- svg/palette.py
+++ svg/palette.py
@@ -3,33 +3,49 @@ from random import randint
 
 
 class Order(str, Enum):
-    STRAIGHT = "straight"
-    SHIFT = "shift"
+    FORWARD = "forward"
+    BACKWARD = "backward"
+    MONO = "mono"
     RANDOM = "random"
+    # SHIFT = "shift"
 
 
 class Palette():
-    def __init__(self, colors: Enum, order: Order = Order.STRAIGHT):
+    def __init__(self, colors: Enum, order: Order):
         self.colors = [c.value for c in colors]
         self.index = 0
         self.max_index = len(self.colors) - 1 
-        self.orger = order
-        if order == Order.SHIFT:
+        self.order = order
+        if order == Order.MONO:
             self.index = randint(0, self.max_index)
 
-    def get_next_color(self):
+    def _get_current_color(self):
+        return self.colors[self.index]
+
+    def _get_next_color(self):
         value = self.colors[self.index]
         self.index = 0 if self.index == self.max_index else self.index + 1
         return value
 
-    def get_random_color(self):
+    def _get_previous_color(self):
+        value = self.colors[self.index]
+        self.index = self.max_index if self.index == 0 else self.index - 1
+        return value
+
+    def _get_random_color(self):
         index = randint(0, self.max_index)
         return self.colors[index]
 
     def get_color(self):
-        if self.orger in [Order.STRAIGHT, Order.SHIFT]:
-            return self.get_next_color()
-        return self.get_random_color()
+        if self.order == Order.MONO:
+            return self._get_current_color()
+        if self.order == Order.FORWARD:
+            return self._get_next_color()
+        if self.order == Order.BACKWARD:
+            return self._get_previous_color()
+        if self.order == Order.RANDOM:
+            return self._get_random_color()
+        raise ValueError(f"{self.order} is invalid value")
 
 
 __all__ = [
